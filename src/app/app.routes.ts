e3de0e2a75d3e0b4d1e3e97bb6e4a71530c03c85
Luis <luis.30551013@uru.edu>
2026-04-09 18:28:29 -0400

    fix: corregir rutas de importacion de los componentes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,13 @@
 import { Routes } from '@angular/router';
+import { Home } from './pages/home/home';
+import { Auth } from './pages/auth/auth';
+import { Projects } from './pages/projects/projects';
+import { ProjectDetail } from './pages/project-detail/project-detail';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: '', component: Home },
+  { path: 'login', component: Auth },
+  { path: 'proyectos', component: Projects },
+  { path: 'proyectos/:id', component: ProjectDetail },
+  { path: '**', redirectTo: '' }
+];
