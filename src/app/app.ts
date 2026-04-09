2c4e84ea79c9601deb1af7b02a858c8670f18888
Luis <luis.30551013@uru.edu>
2026-04-09 18:32:14 -0400

    feat: implementar estructura principal con navbar y footer

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,12 +1,16 @@
-import { Component, signal } from '@angular/core';
+import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+// Ajusta estas rutas si tus archivos se generaron sin el ".component" al final
+import { Navbar } from './shared/navbar/navbar'; 
+import { Footer } from './shared/footer/footer';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  standalone: true,
+  imports: [RouterOutlet, Navbar, Footer], // ¡Añadimos Navbar y Footer aquí!
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
-export class App {
-  protected readonly title = signal('Portafolio-Personal-Manejo-de-Frameworks');
+export class AppComponent {
+  title = 'Portafolio-Personal-Manejo-de-Frameworks';
 }
