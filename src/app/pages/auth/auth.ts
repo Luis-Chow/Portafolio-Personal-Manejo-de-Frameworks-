a98a1074b1a4f1ac08d54ae964281b751f297e5b
Luis <luis.30551013@uru.edu>
2026-04-09 19:04:01 -0400

    feat: crear formulario reactivo para login y registro con validaciones

--- src/app/pages/auth/auth.ts
+++ src/app/pages/auth/auth.ts
@@ -1,9 +1,34 @@
 import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { ReactiveFormsModule, FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-auth',
-  imports: [],
+  standalone: true,
+  imports: [CommonModule, ReactiveFormsModule],
   templateUrl: './auth.html',
-  styleUrl: './auth.css',
+  styleUrl: './auth.css'
 })
-export class Auth {}
+export class Auth {
+  authForm: FormGroup;
+  esRegistro = false;
+
+  constructor(private fb: FormBuilder) {
+    this.authForm = this.fb.group({
+      email: ['', [Validators.required, Validators.email]],
+      password: ['', [Validators.required, Validators.minLength(6)]]
+    });
+  }
+
+  toggleModo() {
+    this.esRegistro = !this.esRegistro;
+    this.authForm.reset();
+  }
+
+  onSubmit() {
+    if (this.authForm.valid) {
+      console.log('Datos enviados:', this.authForm.value);
+      alert(this.esRegistro ? '¡Registro exitoso!' : '¡Inicio de sesión exitoso!');
+    }
+  }
+}
